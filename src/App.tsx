5cefa46d30274f615587e2bcd1e19e937961ecce
asharaam1 <lohanaasharaam1@gmail.com>
2025-09-19 23:38:34 +0500

    UI building

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Auth from "./Auth";
 import TaskManager from "./TaskManager";
 
 export default function App() {
   return (
     <>
-      <TaskManager />
+      {/* <TaskManager /> */}
+      <Auth/>
     </>
   );
 }
--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import bgImg from "./assets/bgimgs.jpg";
 function Auth() {
   return (
-    <div>Auth</div>
-  )
+    <>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundImage: `url(${bgImg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          margin: "0 auto",
+          padding: "1rem",
+          minHeight: "100vh",
+        }}
+      >
+        <div
+          style={{
+            border: "1px solid black",
+            borderRadius: "20px",
+            padding: "0 20px",
+          }}
+        >
+          <h1>Login</h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Auth
+export default Auth;
