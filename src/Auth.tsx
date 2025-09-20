0e591c5bc184a1d6505fd6ed212daf8d56749cac
asharaam1 <lohanaasharaam1@gmail.com>
2025-09-20 19:54:39 +0500

    simple login UI

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -18,12 +18,57 @@ function Auth() {
       >
         <div
           style={{
-            border: "1px solid black",
+            border: "2px solid rgba(20, 67, 208, 1)",
             borderRadius: "20px",
-            padding: "0 20px",
+            padding: "15px",
+            justifyItems: "center",
+            backgroundColor: "rgba(255, 255, 255, 0.2)",
+            backdropFilter: "blur(20px)",
+            WebkitBackdropFilter: "blur(10px)", // for Safari
           }}
         >
-          <h1>Login</h1>
+          <h1 style={{ color: "rgba(20, 67, 208, 1)" }}>Login</h1>
+          <div>
+            <p style={{ margin: "2px" }}>Email:</p>
+            <input
+              type="text"
+              placeholder="email or username"
+              style={{
+                width: "100%",
+                padding: "5px 10px",
+                borderRadius: "20px",
+                marginBottom: "20px",
+                boxSizing: "border-box",
+                border: "2px solid rgba(20, 67, 208, 1)",
+              }}
+            />
+            <p style={{ margin: "2px" }}>Password:</p>
+            <input
+              type="password"
+              placeholder="password"
+              style={{
+                width: "100%",
+                padding: "5px 10px",
+                borderRadius: "20px",
+                marginBottom: "20px",
+                boxSizing: "border-box",
+                border: "2px solid rgba(20, 67, 208, 1)",
+              }}
+            />
+            <button
+              style={{
+                width: "100%",
+                padding: "8px",
+                borderRadius: "20px",
+                color: "white",
+                backgroundImage: `url(${bgImg})`,
+                backgroundSize: "cover",
+                border: "2px solid rgba(20, 67, 208, 1)",
+              }}
+            >
+              Login
+            </button>
+          </div>
         </div>
       </div>
     </>
